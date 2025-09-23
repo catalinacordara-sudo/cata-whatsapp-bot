db1e36b2b0edf0c22b334f2c99624e20c51d29ff
catalinacordara-sudo <catalinacordara@gmail.com>
2025-09-23 11:41:01 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,24 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/webhook", methods=["POST"])
+@app.route("/webhook", methods=["GET","POST"])
 def webhook():
     incoming = (request.values.get("Body", "") or "").strip()
     print(f"💬 Mensaje entrante: {incoming}", flush=True)
 
-    # TwiML de respuesta
     resp = MessagingResponse()
-    if incoming:
-        resp.message(f"Eco: {incoming}")
+    if reqest.method=="GET":
+        # Prueba desde el navegador
+        resp.message(Webhook ok (GET)")
     else:
-        resp.message("Recibí tu mensaje ✅")
+        #Twilio usara POST
+        resp.message("Webhook OK(GET)✅")
+    else:
+        #Twilio usara POST
+        if incoming:
+            resp.message(f"Eco:{incoming}")
+        else:
+            resp.message(Recibi tu mensaje")
 
-    # Devolver SIEMPRE TwiML con el mime-type correcto
-    xml = str(resp)  # <Response><Message>...</Message></Response>
-    return Response(xml, mimetype="application/xml")  # también vale "text/xml"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    xml = str(resp)  
+    return Response(xml, mimetype="application/xml")  
